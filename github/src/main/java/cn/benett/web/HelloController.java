766f49f5626d988c06c6aec8d5b265b66f7ef9bf
benett <chenpeng09@meituan.com>
2017-12-07 19:42:08 +0800

    第一次提交

---
+++ github/src/main/java/cn/benett/web/HelloController.java
@@ -0,0 +1,12 @@
+package cn.benett.web;
+
+/**
+ * Author:chenpeng09@meituan.com
+ * Date: 2017/12/7.
+ * Time: 下午7:40.
+ **/
+public class HelloController {
+    public static void main(String[] args) {
+        System.out.println("hahhhah");
+    }
+}
