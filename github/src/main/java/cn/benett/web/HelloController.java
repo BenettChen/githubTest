95323bf4246b1d8c6e6ff2e16371b6ea7d391d9d
benett <chenpeng09@meituan.com>
2017-12-13 10:54:13 +0800

    你猜我在干嘛

--- github/src/main/java/cn/benett/web/HelloController.java
+++ github/src/main/java/cn/benett/web/HelloController.java
@@ -1,5 +1,12 @@
 package cn.benett.web;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Author:chenpeng09@meituan.com
  * Date: 2017/12/7.
@@ -8,23 +15,35 @@ package cn.benett.web;
 public class HelloController {
     public static void main(String[] args) {
         HelloController hc = new HelloController();
-        hc.fun3();
-        System.out.println("hahhhah");
+        hc.fun();
+//        System.out.println("hahhhah");
     }
 
     private static String getName(){
         return "cp09";
     }
 
-    public void fun(){
-        
+    /**
+     *
+     */
+    private void fun(){
+        List<Integer> list;
+        list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        map.put(1,list);
+        Iterator<Map.Entry<Integer, List<Integer>>> it = map.entrySet().iterator();
+        while (it.hasNext()) System.out.println(it.next());
+        list.add(3);
+        Iterator<Map.Entry<Integer, List<Integer>>> it2 = map.entrySet().iterator();
+        while (it2.hasNext()) System.out.println(it2.next());
+
     }
 
-    public void fun3(){
-        
-    }
+
 
     public void fun2(){
-        System.out.println("本地提交");
+        System.out.println("本地提交ff");
     }
 }
--- github/src/test/java/MyClass.java
+++ github/src/test/java/MyClass.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
 
 /**
  * Author:chenpeng09@meituan.com
@@ -6,4 +6,41 @@ package PACKAGE_NAME;
  * Time: 上午10:13.
  **/
 public class MyClass {
+    public static void main(String[] args) {
+        HashSet<Phone> pSet = new HashSet<>();
+        for (int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                pSet.add(new Phone(i,j));
+            }
+        }
+        System.out.println(pSet.size());
+    }
+
+
+}
+
+class Phone{
+    private int prxNum;
+    private int numLine;
+
+    public Phone(int prxNum, int numLine) {
+        this.prxNum = prxNum;
+        this.numLine = numLine;
+    }
+
+
+//    @Override
+    public boolean equals(Phone o) {
+
+        Phone phone = (Phone) o;
+
+        return numLine == phone.numLine && prxNum != phone.prxNum;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = prxNum;
+        result = 31 * result + numLine;
+        return result;
+    }
 }
